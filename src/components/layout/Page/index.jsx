5ec83dc94bbd9c51db6af13c0f6966cf149bf0ae
T0astBread <tb@t0ast.cc>
2019-01-30 14:52:48 +0100

    Fixed vertical scrolling on chrome

--- src/components/layout/Page/index.jsx
+++ src/components/layout/Page/index.jsx
@@ -4,25 +4,25 @@ import styles from "./styles.css"
 
 const Page = ({ top, topIsSticky = false, left, right, bottom, children, theme = "dark" }) => {
     return (
-        <Box className={`Page Page--theme-${theme}`} fit column>
-            <Box className={`Page__top Page__top--${topIsSticky ? "" : "not-"}sticky`}>
+        <div className={`Page Page--theme-${theme}`} fit column>
+            <div className={`Page__top Page__top--${topIsSticky ? "" : "not-"}sticky`}>
                 {top}
-            </Box>
-            <Box className="Page__body" flexGrow="1">
-                <Box className="Page__left">
+            </div>
+            <div className="Page__body" flexGrow="1">
+                <div className="Page__left">
                     {left}
-                </Box>
-                <Box className="Page__main" flex="1 1 auto">
+                </div>
+                <div className="Page__main" flex="1 1 auto">
                     {children}
-                </Box>
-                <Box className="Page__right">
+                </div>
+                <div className="Page__right">
                     {right}
-                </Box>
-            </Box>
-            <Box className="Page__bottom">
+                </div>
+            </div>
+            <div className="Page__bottom">
                 {bottom}
-            </Box>
-        </Box>
+            </div>
+        </div>
     )
 }
  
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render } from "react-dom"
-import { Page as FillViewport, Box, ScrollView } from "react-layout-components"
+import { Box } from "react-layout-components"
 import HeaderBar from "./components/presentation/HeaderBar"
 import HelloWorld from "./components/presentation/HelloWorld"
 import Page from "./components/layout/Page"
+import FillViewport from "./components/layout/FillViewport"
 import styles from "./styles.css"
 
 const root = document.getElementById("root")
@@ -16,18 +17,18 @@ render((
 					<h1 style={{ color: "var(--primary-1)" }}>Hello world</h1>
 				</HeaderBar>
 			}
-			topIsSticky={false}
+			topIsSticky={true}
 			left={
-				<Box width={400} style={{ backgroundColor: "var(--background-2)", borderRight: "1px solid var(--primary-1)" }} />
+				<Box width={400} style={{ backgroundColor: "var(--background-2)", borderRight: "1px solid var(--primary-1)" }} fit />
 			}
 			right={(
-				<Box width={200} style={{ backgroundColor: "var(--background-2)", borderLeft: "1px solid var(--primary-1)" }} />
+				<Box width={200} style={{ backgroundColor: "var(--background-2)", borderLeft: "1px solid var(--primary-1)" }} fit />
 			)}
 			bottom={(
 				<Box fit height={100} style={{ backgroundColor: "var(--background-2)", borderTop: "1px solid var(--primary-1)" }} />
 			)}>
 			<HelloWorld />
-			{/* <div style={{ height: "5000px" }}>.</div> */}
+			<div style={{ height: "5000px" }}>.</div>
 		</Page>
 	</FillViewport>
 ), root)
